c689456e72cd7f6d221fac2636a25a78fbba1a89
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2023-05-11 17:42:33 -0400

    functioning nav bars, main landing page, and top bar added. Responsive and toggles fine

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -15,7 +15,30 @@ var swiper = new Swiper(".swiperIntro", {
   },
 });
 
-function toggleSidebar() {
-  var sidebar = document.querySelector(".sidebar");
-  sidebar.classList.toggle("open");
+// Socials navigation +
+function toggleNavSocials() {
+  const navMenu = document.querySelector(".nav-socials");
+
+  const xSocials = document.getElementById("plus-icon-socials");
+
+  navMenu.classList.toggle("openSocials");
+
+  if (navMenu.classList.contains("openSocials")) {
+    xSocials.style.transform = "rotate(45deg)";
+  } else {
+    xSocials.style.transform = "rotate(90deg)";
+  }
+}
+// Pages/links navigation +
+function toggleNavPages() {
+  const navMenu = document.querySelector(".nav-pages");
+  const xPages = document.getElementById("plus-icon-pages");
+
+  navMenu.classList.toggle("openPages");
+
+  if (navMenu.classList.contains("openPages")) {
+    xPages.style.transform = "rotate(45deg)";
+  } else {
+    xPages.style.transform = "rotate(90deg)";
+  }
 }
