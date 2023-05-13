1182b4f9b6044154486c840d937d288c3196ce96
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2023-05-13 10:54:20 -0400

    reorganized swiper for projects into javascript array of objects for easy additions down the road. Swiper will now populate based on template literals and I hadded conditional that if href is blank, hide the button

--- assets/js/dataProjects.js
+++ assets/js/dataProjects.js
@@ -1,4 +1,58 @@
 const projects = [
-  // sunnyD
-  {},
+  {
+    imgSrc: "assets/images/sunnyd-seltzers.png",
+    alt: "image of sunnyd cocktails website",
+    h2ProjectName: "SunnyD Seltzers",
+    buttonProjectLink: "/sunnyd-cocktails.html",
+    buttonText: "view project",
+    buttonRepoLink: "",
+    buttonRepoText: "",
+  },
+  {
+    imgSrc: "assets/images/funfetti.jpg",
+    alt: "image of funfetti website",
+    h2ProjectName: "Funfetti",
+    buttonProjectLink: "/funfetti.html",
+    buttonText: "view project",
+    buttonRepoLink: "",
+    buttonRepoText: "",
+  },
+  {
+    imgSrc: "assets/images/honeygo-pediatrics.jpg",
+    alt: "image of Honeygo Pediatrics website",
+    h2ProjectName: "Honeygo Pediatrics",
+    buttonProjectLink: "/honeygo-pediatrics.html",
+    buttonText: "view project",
+    buttonRepoLink: "/",
+    buttonRepoText: "github",
+  },
+  {
+    imgSrc: "assets/images/career-prep.png",
+    alt: "image of Career prep and professional development website",
+    h2ProjectName: "Career Prep and Professional Development <br> coming soon",
+    buttonProjectLink: "",
+    buttonText: "Coming Soon",
+    buttonRepoLink: "/",
+    buttonRepoText: "github",
+  },
 ];
+
+for (var i = 0; i < projects.length; i++) {
+  document.querySelector(".swiper-wrapper").innerHTML += `
+<div class="swiper-slide">
+    <img src="${projects[i].imgSrc}" alt="${projects[i].alt}"> 
+    <div>
+        <h2>${projects[i].h2ProjectName}</h2>
+        <a class="button" href="${projects[i].buttonProjectLink}">${projects[i].buttonText}</a>
+        <a class="button" href="${projects[i].buttonRepoLink}">${projects[i].buttonRepoText}</a>
+        </div>
+    </div>`;
+}
+// Hide all buttons if href="" is blank
+const buttons = document.getElementsByClassName("button");
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
+  if (button.getAttribute("href") === "") {
+    button.style.display = "none";
+  }
+}
