c56c2031e37be16c7a533d5294f747f4b19b6220
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2023-05-13 08:20:18 -0400

    split up and organized js files

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,30 +1,4 @@
-// Socials navigation +
-function toggleNavSocials() {
-  const navMenu = document.querySelector(".nav-socials");
-
-  const xSocials = document.getElementById("plus-icon-socials");
-
-  navMenu.classList.toggle("openSocials");
-
-  if (navMenu.classList.contains("openSocials")) {
-    xSocials.style.transform = "rotate(45deg)";
-  } else {
-    xSocials.style.transform = "rotate(90deg)";
-  }
-}
-// Pages/links navigation +
-function toggleNavPages() {
-  const navMenu = document.querySelector(".nav-pages");
-  const xPages = document.getElementById("plus-icon-pages");
-
-  navMenu.classList.toggle("openPages");
-
-  if (navMenu.classList.contains("openPages")) {
-    xPages.style.transform = "rotate(45deg)";
-  } else {
-    xPages.style.transform = "rotate(90deg)";
-  }
-}
+// Swiper for projects
 var swiper = new Swiper(".swiper-all-projects", {
   slidesPerView: "1",
   centeredSlides: true,
