ac78fc7a3f25054d5a3de0c3ba8a652aaaa7e8ff
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-03-27 15:33:01 -0600

    set up js build to reduce abundance of code and better organize

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+import Accessibility from "accessibility";
+
+import "./footer-component.js";
+import "./contact-component.js";
+import "./contact-component.js";
+import "./swiper.js";
+import "./dataProjects.js";
+import "./copyright.js";
