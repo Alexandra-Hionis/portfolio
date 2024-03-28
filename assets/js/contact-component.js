650e5cfad1048885ffd47952baffeea9f9164f18
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-03-28 11:25:07 -0600

    added project

--- assets/js/contact-component.js
+++ assets/js/contact-component.js
@@ -7,7 +7,7 @@ class Contact extends HTMLElement {
     <h1>Contact</h1>
     <hr size="2" color="black" />
     <h2>Have a question or want to work together?</h2>
-    <p> Please feel free to reach out! You can find me on Upwork for any Freelance request. 
+    <p> Please feel free to reach out! You can find me on <a href="https://www.upwork.com/freelancers/~01f4c77eb5abe7904e" target="_blank"><u>Upwork</u></a> for any Freelance request. 
       <br>You can also reach out via email. I typically respond within 24 hours.</p>
       <a href="mailto:alexandra.hionis@gmail.com">
       <div class="contact__info">
