7e70017e20db10d96cf86bcf35d6f7428eab4bc3
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-03-28 11:09:40 -0600

    tart script to concurrently run JavaScript watch and server processes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,3 @@
-// const path = require("path");
-
-// module.exports = {
-//   entry: {
-//     // bundle: "./assets/js/index.js",
-//     file1: "./assets/js/footer-component.js",
-//     file2: "./assets/js/contact-component.js",
-//     file3: "./assets/js/nav.js",
-//     file4: "./assets/js/swiper.js",
-//     file5: "./assets/js/dataProjects.js",
-//     file6: "./assets/js/copyright.js",
-//   },
-//   output: {
-//     path: path.resolve(__dirname, "dist/js"),
-//     filename: "[name].bundle.js",
-//   },
-// };
-
 const path = require("path");
 
 module.exports = {
