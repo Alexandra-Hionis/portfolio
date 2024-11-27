31f3448fd4b77754760f3da4a35cc71e9a26b1ac
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-11-26 21:58:14 -0500

    404 page

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
-// app.use(express.static(__dirname + "/src/styles.css"));
+
 // sendFile will go here
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// Custom 404 handler
+app.use(function (req, res, next) {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
 });
 
 app.listen(port);
